399259d8c48da132ca2c09bfac5ff83d5f0c4d91
Tri Coder <Anhtri22303@gmail.com>
2024-06-30 14:28:27 +0700

    .

--- src/main/java/com/jewelry_store/jewelry_store/config/AppConfig.java
+++ src/main/java/com/jewelry_store/jewelry_store/config/AppConfig.java
@@ -43,7 +43,7 @@ public class AppConfig {
 
                 cfg.setAllowedOrigins(Arrays.asList(
                         "http://localhost:3000",
-                        "https://jewelry-store-djlxzrjid-anhtri22303s-projects.vercel.app/"));
+                        "https://jewelry-store-app.vercel.app/"));
                 cfg.setAllowedMethods(Collections.singletonList("*"));
                 cfg.setAllowCredentials(true);
                 cfg.setAllowedHeaders(Collections.singletonList("*"));
