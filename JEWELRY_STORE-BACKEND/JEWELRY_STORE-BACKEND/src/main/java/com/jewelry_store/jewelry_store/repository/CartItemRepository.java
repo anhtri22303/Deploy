0a1336eb56ee3f69a129e5357b739cd7c200b339
Tri Coder <Anhtri22303@gmail.com>
2024-06-30 11:08:10 +0700

    .

--- JEWELRY_STORE-BACKEND/JEWELRY_STORE-BACKEND/src/main/java/com/jewelry_store/jewelry_store/repository/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jewelry_store.jewelry_store.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jewelry_store.jewelry_store.model.CartItem;
-
-public interface CartItemRepository extends JpaRepository<CartItem,Long> {
-
-}
