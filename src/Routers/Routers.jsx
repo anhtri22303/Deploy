10cebe330a5c0a7f23b7939a8ee82362f1d37336
Anh Trí <Anhtri22303@gmail.com>
2025-02-06 21:58:41 +0700

    .

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -15,10 +15,10 @@ const Routers = () => {
     const { auth } = useSelector((store) => store);
     return (
         <Routes>
-            <Route path="/" element={<LoginForm/>} />
-            <Route path="/staff/jewelry/*" element={<StaffRoute/>} />
-            <Route path="/manager/jewelry/*" element={<ManagerRoute />} />
-            <Route path="/admin/jewelry/*" element={<AdminRoute/>} />
+            <Route path="/" element={<AdminRoute/>} />
+            {/* <Route path="/staff/jewelry/*" element={<StaffRoute/>} />
+            <Route path="/manager/jewelry/*" element={<ManagerRoute />} /> */}
+            {/* <Route path="/admin/jewelry/*" element={<AdminRoute/>} /> */}
 
         </Routes>
     )
